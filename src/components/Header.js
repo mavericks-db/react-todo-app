2f76f9e8683b4b6b1b729bcd7ff25efffd280447
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-26 18:27:07 +0500

    Update the heading element to include the style attribute inside the Header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 function Header() {
   return (
-    <header>
+    <header
+      style={{
+        fontSize: '6rem',
+        fontWeight: '600',
+        marginBottom: '2rem',
+        lineHeight: '1em',
+        color: '#ececec',
+        textTransform: 'lowercase',
+        textAlign: 'center',
+      }}
+    >
       <h1>todos</h1>
     </header>
   );
