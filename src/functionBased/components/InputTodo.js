3e4fc4856606a04a57542eb995e7bb33b467b057
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-27 16:36:43 +0500

    Delete all the code in the InputTodo.js file

--- src/functionBased/components/InputTodo.js
+++ src/functionBased/components/InputTodo.js
@@ -1,57 +1,5 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-class InputTodo extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      title: '',
-    };
-  }
-
-  onChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const { addTodoProps } = this.props;
-    const { title } = this.state;
-    if (title.trim()) {
-      addTodoProps(title);
-      this.setState({
-        title: '',
-      });
-    } else {
-      alert('Please write item');
-    }
-  };
-
-  render() {
-    const { title } = this.state;
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add Todo..."
-          value={title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <button type="submit" className="input-submit">
-          Submit
-        </button>
-      </form>
-    );
-  }
-}
-
-InputTodo.propTypes = {
-  addTodoProps: PropTypes.func.isRequired,
-};
+const InputTodo = () => <div />;
 
 export default InputTodo;
