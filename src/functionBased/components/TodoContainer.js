9da7fc231a6d52d97d5cc42a4be98494f752193f
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-27 17:41:19 +0500

    Assign the function to the state variable

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -5,7 +5,14 @@ import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 const TodoContainer = () => {
-  const [todos, setTodos] = useState([]);
+  function getInitialTodos() {
+    // getting stored items
+    const temp = localStorage.getItem('todos');
+    const savedTodos = JSON.parse(temp);
+    return savedTodos || [];
+  }
+
+  const [todos, setTodos] = useState(getInitialTodos());
 
   const handleChange = (id) => {
     setTodos((prevState) => prevState.map((todo) => {
@@ -45,25 +52,6 @@ const TodoContainer = () => {
       }),
     );
   };
-
-  // useEffect(() => {
-  //   console.log('test run');
-
-  //   // getting stored items
-  //   const temp = localStorage.getItem('todos');
-  //   const loadedTodos = JSON.parse(temp);
-
-  //   if (loadedTodos) {
-  //     setTodos(loadedTodos);
-  //   }
-  // }, []);
-
-  function getInitialTodos() {
-    // getting stored items
-    const temp = localStorage.getItem('todos');
-    const savedTodos = JSON.parse(temp);
-    return savedTodos || [];
-  }
 
   useEffect(() => {
     // storing todos items
