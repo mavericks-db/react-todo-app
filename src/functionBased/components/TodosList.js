0a99d1199deaaa4af4a0db3d882461249ce69840
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-29 21:03:48 +0500

    Convert TodosList.js class based component to a function-based component

--- src/functionBased/components/TodosList.js
+++ src/functionBased/components/TodosList.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-class TodosList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    const {
-      todos, handleChangeProps, deleteTodoProps, setUpdate,
-    } = this.props;
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={handleChangeProps}
-            deleteTodoProps={deleteTodoProps}
-            setUpdate={setUpdate}
-          />
-        ))}
-      </ul>
-    );
-  }
-}
+const TodosList = (props) => {
+  const {
+    todos, handleChangeProps, deleteTodoProps, setUpdate,
+  } = props;
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={handleChangeProps}
+          deleteTodoProps={deleteTodoProps}
+          setUpdate={setUpdate}
+        />
+      ))}
+    </ul>
+  );
+};
 
 TodosList.propTypes = {
   todos: PropTypes.arrayOf(
