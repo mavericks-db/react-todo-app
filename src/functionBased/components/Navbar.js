dd0f9b7c4f8aed8a40902bdbc689c40df960b31a
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-30 22:05:46 +0500

    Import the Link from the react-router-dom in the Navbar component

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const links = [
@@ -16,7 +17,9 @@ const Navbar = () => {
   return (
     <nav className="navBar">
       <ul>
-        {links.map((link) => <li key={link.id}>{link.text}</li>)}
+        {links.map((link) => (
+          <li key={link.id}>{link.text}</li>
+        ))}
       </ul>
     </nav>
   );
