29b27ae167b2ddcefc7777a263d33ec98730cf73
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-30 22:02:48 +0500

    Start by adding an array of objects (containing all your links items) above the return statement in the Navbar component

--- src/functionBased/components/Navbar.js
+++ src/functionBased/components/Navbar.js
@@ -1,4 +1,18 @@
 import React from 'react';
 
-const Navbar = () => <div>Hello from Navbar</div>;
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ];
+    <div>Hello from Navbar</div>;
+};
 export default Navbar;
