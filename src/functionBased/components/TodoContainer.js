aef3e28548f445dfbdabaf4078127463d3729b99
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-30 21:59:44 +0500

    Render its instance above the <Routes> element

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -64,27 +64,30 @@ const TodoContainer = () => {
   }, [todos]);
 
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={(
-          <div className="container">
-            <div className="inner">
-              <Header />
-              <InputTodo addTodoProps={addTodoItem} />
-              <TodosList
-                todos={todos}
-                handleChangeProps={handleChange}
-                deleteTodoProps={delTodo}
-                setUpdate={setUpdate}
-              />
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <div className="container">
+              <div className="inner">
+                <Header />
+                <InputTodo addTodoProps={addTodoItem} />
+                <TodosList
+                  todos={todos}
+                  handleChangeProps={handleChange}
+                  deleteTodoProps={delTodo}
+                  setUpdate={setUpdate}
+                />
+              </div>
             </div>
-          </div>
-        )}
-      />
-      <Route path="/about" element={<About />} />
-      <Route path="*" element={<NotMatch />} />
-    </Routes>
+          )}
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotMatch />} />
+      </Routes>
+    </>
   );
 };
 
